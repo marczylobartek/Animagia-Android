3dfca8bf546600b16593c2183c931ee0d4ae5437
marczylobartek <marczylo.b@gmail.com>
2019-04-25 15:21:56 +0200

    bug solved

--- app/src/main/java/pl/animagia/ContactInfoFragment.java
+++ app/src/main/java/pl/animagia/ContactInfoFragment.java
@@ -38,4 +38,56 @@ public class ContactInfoFragment extends Fragment {
         return frame;
     }
 
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //Cookies.removeCookie(Cookies.LOGIN, getActivity());
+
+        Button fbButton = getView().findViewById(R.id.fbButton);
+        fbButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://fb.com/WydawnictwoAnimagia"));
+                getActivity().startActivity(browserIntent);
+            }
+        });
+
+        Button emailButton = getView().findViewById(R.id.emailButton);
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"animagia@animagia.pl"});
+                i.putExtra(Intent.EXTRA_SUBJECT, "Temat wiadomości mail");
+                i.putExtra(Intent.EXTRA_TEXT   , "Treść wiadomości mail");
+                i.setDataAndType(Uri.parse("mailto:"),"message/rfc822" );
+                try {
+                    startActivity(Intent.createChooser(i, "Wybierz aplikację..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(getActivity(), "Nie ma zainstalowanej aplikacji do wysyłania maila.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        final TextView signalNumber = getView().findViewById(R.id.signalNumber);
+        ImageView signal = getView().findViewById(R.id.signalCopy);
+        signal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    ClipboardManager clipboard = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                    ClipData clip = ClipData.newPlainText("Skopiowany..", signalNumber.getText());
+                    clipboard.setPrimaryClip(clip);
+                }catch(NullPointerException e){
+
+                }
+
+            }
+        });
+    }
+
+
+
 }
